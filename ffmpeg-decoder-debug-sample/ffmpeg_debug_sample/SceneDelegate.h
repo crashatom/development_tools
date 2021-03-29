23ceb2283f93fac7da06a9a44bb833b50b9eccf6
crashatom <445335670@qq.com>
2021-03-29 22:54:30 +0800

    Update SceneDelegate.h

--- ffmpeg-decoder-debug-sample/ffmpeg_debug_sample/SceneDelegate.h
+++ ffmpeg-decoder-debug-sample/ffmpeg_debug_sample/SceneDelegate.h
@@ -1,11 +1,3 @@
-//
-//  SceneDelegate.h
-//  ffmpeg_debug_sample
-//
-//  Created by Jim on 2019/10/25.
-//  Copyright © 2019 Tuya. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 
 @interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
