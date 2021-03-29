a7b2be46de30250a6fccec20e6b1c8a67eed7b0d
crashatom <445335670@qq.com>
2021-03-29 22:55:00 +0800

    Update AppDelegate.h

--- ffmpeg-decoder-debug-sample/ffmpeg_debug_sample/AppDelegate.h
+++ ffmpeg-decoder-debug-sample/ffmpeg_debug_sample/AppDelegate.h
@@ -1,11 +1,3 @@
-//
-//  AppDelegate.h
-//  ffmpeg_debug_sample
-//
-//  Created by Jim on 2019/10/25.
-//  Copyright © 2019 Tuya. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
